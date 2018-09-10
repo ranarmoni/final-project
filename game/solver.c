6fee71077d8162e94423b4ab4990253861a05193
shgoren <34242458+shgoren@users.noreply.github.com>
2018-09-11 00:46:36 +0300

    backtracker

--- game/solver.c
+++ game/solver.c
@@ -1,6 +1,7 @@
-#include "game.h"
+#include "solver.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int BLOCK_SIZE_N;
 int BLOCK_SIZE_M;
@@ -16,6 +17,11 @@ typedef struct {
     Node *top;
     int size;
 } Stack;
+
+void initialise(Stack *s);
+void push(Stack *s, int indx, int val);
+void pop(Stack *s);
+int getTop(Stack *s, int getIndex);
 
 void initialise(Stack *s){
     s->size = 0;
@@ -40,7 +46,6 @@ void push(Stack *s, int indx, int val){
 }
 
 void pop(Stack *s){
-    int temp;
     Node *np;
 
     if (s->top == NULL) {
@@ -54,6 +59,8 @@ void pop(Stack *s){
 }
 
 int getTop(Stack *s, int getIndex){
+	if(s->top == NULL)
+		return -1;
 	if(getIndex == 1)
 		return s->top->index;
 	else
@@ -64,23 +71,65 @@ void setSeed(int seed){
 	srand(seed);
 }
 
-int countNuberOfSols(GameBoard *originalBoard){
+int countNumberOfSols(GameBoard *originalBoard){
+	/* GameBoard tempBoard; */
 	int* currBoard = (int*)calloc(TABLE_SIZE*TABLE_SIZE*3,sizeof(int));
-	int count = 0, i;
-	Stack stack = (Stack*)calloc(sizeof(Stack));
+	int count = 0, i, currIndex = 0, currValue;
+	Stack s, *stack = &s;
 
     if (!currBoard) {
 		printf("Error: calloc has failed\n");
 		exit(0);
     }
-
-	memcpy((void*)originalBoard->board,(void*)currBoard);
+/*
+    tempBoard.board = currBoard;
+*/
+	memcpy((void*)originalBoard->board,(void*)currBoard, TABLE_SIZE*TABLE_SIZE*3);
 
 	initialise(stack);
 	do{
-		for(i=0;i<TABLE_SIZE;i++){
-
+		if(currIndex > 8){
+/*		if(currIndex > calcIndex(TABLE_SIZE,TABLE_SIZE,2,TABLE_SIZE,3)){*/ /*solution found if current index is larger than the array */
+			count++;
+			printIntArr(currBoard,9);
 		}
+		else if(currBoard[currIndex] == 0){
+			/* push all legal values to stack */
+			push(stack, currIndex, 0);
+			for(i=1;i<=TABLE_SIZE;i++){
+			/*	if(isLegalSet(&tempBoard, (currIndex/3)/TABLE_SIZE , (currIndex/3)%TABLE_SIZE , i)) */
+				if(1)
+					push(stack, currIndex, i);
+			}
+		}
+		else{
+			currIndex += 3;
+			continue;
+		}
+		currIndex = getTop(stack,1);
+		currValue = getTop(stack, 0);
+		currBoard[currIndex] = currValue;
+		/* if out of options reset board until new move */
+		while(currValue == 0){
+			pop(stack);
+			currIndex = getTop(stack,1);
+			currValue = getTop(stack, 0);
+			currBoard[currIndex] = currValue;
+		}
+		if(stack->top != NULL)
+			pop(stack);
+		currIndex += 3;
 	}while(stack->top != NULL);
 	return count;
+}
+
+int main(){
+	GameBoard boardel;
+	int mat[9] = {0,0,0,0,0,0,0,0,0};
+	TABLE_SIZE = 3;
+	boardel.board = mat;
+	printf("%d solutions\n",countNumberOfSols(&boardel));
+
+
+	return 1;
 }
--- game/solver.h
+++ game/solver.h
@@ -9,8 +9,8 @@
 #define SOLVER_H_
 
 #include "game.h"
-#include <time.h>
 #include <stdlib.h>
+#include "MainAux.h"
 
 /*
  * seed to be set by main for randomization
@@ -19,18 +19,7 @@ void setSeed();
 
 
 
-GameBoard* generateSolution(GameBoard *board);
-
-/*
- * we need to make sure that fixed it is initialized to 0 unless really fixed
- */
-GameBoard* generateBoard(GameBoard *solution, GameBoard *board, int fixedAmnt);
-
-
-/*
- * function needs to supply new solution + print appropriate message.
- */
-GameBoard* hasSolution(GameBoard *board);
+int countNumberOfSols(GameBoard *originalBoard);
 
 
 
